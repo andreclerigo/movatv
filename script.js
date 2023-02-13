491650fbc43913f14938ac0acd67f38a4cb3342f
andreclerigo <andreclerigo@outlook.com>
2023-02-13 00:59:20 +0000

    adding intro video

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the page elements
+    var videoFrame = document.getElementById("videoFrame");
+    var video = document.getElementById("video");
+    var content = document.getElementById("content");
+    var footer = document.getElementById("footer");
+
+    // Hide the page elements
+    content.style.display = "none";
+    for (const child of content.children) child.style.display = "none";
+    footer.style.display = "none";
+
+    // Show the video
+    video.style.display = "block";
+    video.play();
+    document.body.style.overflow = "hidden";
+
+    // Show the page elements when the video ends
+    video.onended = function() {
+        console.log('acabou');
+        
+        // Remove the video element
+        video.remove();
+        video.style.display = "none";
+        videoFrame.style.display = "none";
+        
+        // Show the page elements
+        content.style.display = "block";
+        for (const child of content.children) child.style.display = "block";
+        footer.style.display = "block";
+    };
+});
